196390f19f98de3b26a25c0f8d0c4557c0659c59
otabeksh <103041366+otabeksh@users.noreply.github.com>
2022-04-05 16:38:51 +0500

    Add files via upload

---
+++ web_tech_cw/README.md
@@ -0,0 +1,39 @@
+# NOTE TAKING APPLICATION
+
+You can take daily notes with the help of this app
+
+#### Instructions
+
+1. Download the source code
+
+```bash
+git clone https://github.com/wiutstudent2/web_tech_cw.git
+```
+
+or you can download the .zip file
+
+2. Install dependencies
+
+```bash
+npm install
+```
+
+3. Run the app
+
+```bash
+node index
+```
+
+### Dependencies List
+
+- express
+- sqlite3
+- pug
+
+### Link to github repo
+
+https://github.com/wiutstudent2/web_tech_cw.git
+
+### Link to the Website URL
+
+https://amethyst-handsome-aragon.glitch.me/
---
+++ web_tech_cw/index.js
@@ -0,0 +1,121 @@
+let express = require('express');
+let app = express();
+let sqlite3 = require('sqlite3');
+
+app.use(express.urlencoded({ extended: true }));
+app.set('view engine', 'pug');
+app.use('/static', express.static('public'));
+
+const db = new sqlite3.Database('./notes.db', sqlite3.OPEN_READWRITE, (err) => {
+  if (err) console.log(err.message);
+
+  console.log('Connection successful!');
+});
+
+// db.run(
+//   'CREATE TABLE notes (id INTEGER PRIMARY KEY, description VARCHAR(255), archived int)'
+// );
+
+app.get('/home', (req, res) => {
+  res.render('home');
+});
+
+app.get('/', (req, res) => {
+  res.render('home');
+});
+
+app.get('/notes', (req, res) => {
+  db.all('select * FROM notes WHERE archived = 0', [], (err, rows) => {
+    if (err) console.log(err.message);
+
+    res.render('notes', { notes: rows });
+  });
+});
+
+app.get('/create', (req, res) => {
+  res.render('create');
+});
+
+app.post('/create', (req, res) => {
+  const userInput = req.body;
+
+  if (userInput.note.length === 0) {
+    res.render('create', { fail: true });
+  } else {
+    db.run(
+      'insert into notes(description, archived) values (?, 0)',
+      [userInput.note],
+      (err) => {
+        if (err) console.log(err.message);
+
+        db.all('select * FROM notes WHERE archived = 0', [], (err, rows) => {
+          if (err) console.log(err.message);
+
+          res.render('notes', { notes: rows });
+        });
+      }
+    );
+  }
+});
+
+app.get('/notes/:id/deleteunarchived', (req, res) => {
+  id = req.params.id;
+
+  db.run('DELETE FROM notes WHERE id=?', id, (err) => {
+    if (err) console.log(err.message);
+
+    res.redirect('/notes');
+  });
+});
+
+app.get('/notes/:id/UPDATE', (req, res) => {
+  db.get('select * FROM notes WHERE id = ?', req.params.id, (err, row) => {
+    res.render('edit', { id: req.params.id, note: row });
+  });
+});
+
+app.post(`/notes/:id/update`, (req, res) => {
+  let userInput = req.body;
+
+  if (userInput.note.length === 0) {
+    db.get('select * FROM notes WHERE id = ?', req.params.id, (err, row) => {
+      res.render('edit', { id: req.params.id, note: row, fail: true });
+    });
+  } else {
+    db.run(
+      'UPDATE notes set description = ? WHERE id = ?',
+      [userInput.note, req.params.id],
+      (err) => {
+        if (err) console.log(err.message);
+      }
+    );
+
+    res.redirect('/notes');
+  }
+});
+
+app.get('/archive', (req, res) => {
+  db.all('SELECT * FROM notes WHERE archived = 1', [], (err, rows) => {
+    if (err) console.log(err.message);
+
+    res.render('archive', { notes: rows });
+  });
+});
+
+app.get('/notes/:id/archive', (req, res) => {
+  db.run('UPDATE notes set archived = 1 WHERE id = ?', req.params.id, (err) => {
+    if (err) console.log(err.message);
+
+    res.redirect('/notes');
+  });
+});
+
+app.get('/notes/:id/unarchive', (req, res) => {
+  db.run('UPDATE notes set archived = 0 WHERE id = ?', req.params.id, (err) => {
+    if (err) console.log(err.message);
+
+    res.redirect('/archive');
+  });
+});
+
+app.listen(3000);
